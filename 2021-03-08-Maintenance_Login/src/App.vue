84c650f95df50bc76d2f6c76e7a7ae7c3d83950a
cofaSong <leo01817@gmail.com>
2021-03-08 16:01:07 +0800

    login change

---
+++ 2021-03-08-Maintenance_Login/src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Maintenance_System"/>
+    <vue-particles
+      class="backgroundimg"
+      color="#dedede"
+      :particleOpacity="0.7"
+      :particlesNumber="80"
+      shapeType="circle"
+      :particleSize="4"
+      linesColor="#dedede"
+      :linesWidth="1"
+      :lineLinked="true"
+      :lineOpacity="0.4"
+      :linesDistance="150"
+      :moveSpeed="3"
+      :hoverEffect="true"
+      hoverMode="grab"
+      :clickEffect="true"
+      clickMode="push"
+    ></vue-particles>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+body{
+  margin: 0;
+}
+@media (max-width:500px) {
+  h1{
+    font-size: 20px;
+  }
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.backgroundimg{
+  position: absolute;
+  top: 0;
+  z-index: -20;
+  width: 100vw;
+  height: 100vh;
+  min-height: 450px;
+  background-color: rgb(86, 85, 150);
+  animation-name: change_color;
+  animation-duration: 30s;
+  animation-iteration-count: infinite;
+}
+@keyframes change_color{
+    20%{
+        background-color: rgb(47, 150, 168);
+    }
+    40%{
+        background-color: rgb(104, 155, 98);
+    }
+    60%{
+        background-color: rgb(168, 82, 67);
+    }
+    80%{
+        background-color: rgb(185, 121, 173);
+    }
+}
+</style>
---
+++ 2021-03-08-Maintenance_Login/src/components/HelloWorld.vue
@@ -0,0 +1,93 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="signinbox">
+      <div class="signinbg"></div>
+      <img class="qrcode" src="../assets/QrCode.png">
+      <button class="loginbtn"><a :href="signin()">訪客登入</a></button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  methods: {
+    signin(){
+      return 'http://www.fmsbolymin.com.tw:8813/repair/#/login?90018;Cofa;Bolymin'
+    }
+  }
+}
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: whitesmoke;
+  text-decoration: none;
+  transition: 0.7s;
+}
+a:visited {
+  color: whitesmoke;
+}
+a:hover {
+  color: rgb(53, 53, 53);
+}
+.hello{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.signinbox{
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  width: 33vw;
+  min-width: 300px;
+  height: 40vh;
+  min-height: 300px;
+}
+.signinbg{
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgb(224, 224, 224);
+  opacity: 0.7;
+  border-radius: 15px;
+}
+.qrcode{
+  z-index: 30;
+  width: 150px;
+  height: 150px;
+}
+.loginbtn{
+  width: 150px;
+  height: 50px;
+  border-radius: 6px;
+  z-index: 20;
+  font-size: 20px;
+  font-weight: bold;
+  color: whitesmoke;
+  transition: 0.7s;
+  background-color: rgb(50, 106, 158);
+}
+.loginbtn:hover{
+  background-color: rgb(96, 226, 243);
+}
+</style>
